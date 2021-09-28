edcbc306b2773ef21aedc7303ffdf2f11cc1795b
AvadheshSingh <80583229+AvadheshSingh@users.noreply.github.com>
2021-09-28 16:19:07 +0530

    new
    new

---
+++ js/slider.js
@@ -0,0 +1,126 @@
+$('.hero-slider').owlCarousel({
+    loop: true,
+    autoplay: true,
+    margin: 10,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
+$('.best-sellers-card').owlCarousel({
+    loop: true,
+    autoplay: true,
+    dots: false,
+    nav: true,
+    margin: 10,
+    responsive: {
+        0: {
+            items: 2
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
+$(".owl-prev").html('<i class="fa fa-chevron-left"></i>');
+$(".owl-next").html('<i class="fa fa-chevron-right"></i>');
+$('.best-sellers-card1').owlCarousel({
+    loop: true,
+    autoplay: true,
+    dots: false,
+    nav:false,
+    margin: 10,
+    responsive: {
+        0: {
+            items: 2
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
+
+$('.feature-products-card').owlCarousel({
+    loop: true,
+    autoplay: true,
+    dots: false,
+    nav: true,
+    margin: 10,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        700: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
+})
+$(".owl-prev").html('<i class="fa fa-chevron-left"></i>');
+$(".owl-next").html('<i class="fa fa-chevron-right"></i>');
+
+
+$('.feature-products-card1').owlCarousel({
+    loop: true,
+    autoplay: true,
+    dots: false,
+    nav: false,
+    margin: 10,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        700: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
+})
+
+
+$('.user-list-card').owlCarousel({
+    loop: true,
+    autoplay: true,
+    dots: false,
+    nav: true,
+    margin: 10,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+      
+        1000: {
+            items: 2
+        }
+    }
+})
+
+
+
+
